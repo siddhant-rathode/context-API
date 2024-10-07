2ee98f3d0961c348bde49e7048a28b73279699b4
Siddhant Rathode <siddhantrathode@gmail.com>
2024-10-07 23:48:02 +0530

    vite-boiler

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header" 
+import Nav from "./components/Nav"
+
+const App = () => {
+  return (
+    <>
+    <div className='text-3xl bg-gray-500'>hello</div>
+    <Header/>
+    <Nav/>
+    </>
+  )
+}
+
+export default App
