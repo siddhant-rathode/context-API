1763bee93f8f2679446ec3dee4a88fc9ddbecdf9
Siddhant Rathode <siddhantrathode@gmail.com>
2024-10-13 23:50:33 +0530

    normal

--- src/utils/Wrapper.jsx
+++ src/utils/Wrapper.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react"
 export const todocontext = createContext(null);
 
 const Wrapper = (props) => {
-    const [data, setData] = useState("important data");
+    const [data, setData] = useState("some important data");
   return (
     <todocontext.Provider value={[data, setData]}>
         {props.children}
